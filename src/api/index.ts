63ce42b9579b0b1e36d2e648ba59aba53945067e
gabriel-Oak <gabrielcarvalhocosta@live.com>
2024-04-01 15:18:57 -0300

    feat: vercel build

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,13 @@
 import { FastifyInstance } from 'fastify';
-import createContainer from '../src/core/utils/decorators/container';
-import { ILoggerService } from '../src/core/utils/services/logger-service/types';
-import startServer from '../src/fastify/server';
+import createContainer from '../core/utils/decorators/container';
+import { ILoggerService } from '../core/utils/services/logger-service/types';
+import startServer from '../fastify/server';
 
 export default async function handler(req: any, reply: any) {
   const container = createContainer();
   const logger = container.get<ILoggerService>('ILoggerService');
   const app = await startServer()
-    .catch((error) => logger.error(error.message ?? error, error)) as FastifyInstance;
+    .catch((error: any) => logger.error(error.message ?? error, error)) as FastifyInstance;
 
   app.server.emit('request', req, reply);
 }
